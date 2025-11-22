74f56d717bd68424f7720d7cec8f560d618ed39a
Lance Bailey <lancebailey222@gmail.com>
2025-11-22 00:52:45 -0500

    add title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Nav from '@/components/nav';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <title>Mythic Media</title>
       <body><Nav />{children}</body>
     </html>
   );
