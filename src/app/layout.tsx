df9eae540947c41f00769350d239e19e80f3a4cf
Lance Bailey <lancebailey222@gmail.com>
2025-11-23 17:06:23 -0500

    remove nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import '../../styles/globals.css';
 import { ReactNode } from 'react';
-import Nav from '@/components/nav';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <title>Mythic Media</title>
-      <body><Nav />{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
